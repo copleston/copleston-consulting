28ae2f360ec0985078ba6e5c721e7f492ca20aa6
Oliver Copleston <o.copleston@gmail.com>
2023-08-14 22:33:24 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://copleston.github.io',
+  site: 'https://cop-con.com',
   base: '/copleston-consulting',
 });
